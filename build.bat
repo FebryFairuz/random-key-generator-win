f309cc8a1b1cae82a097d71a61169e81bd4351ca
Equan Pr <eq@junwatu.com>
2013-04-24 15:28:03 +0700

    build batch script.

---
+++ build.bat
@@ -0,0 +1,33 @@
+@ECHO OFF
+ECHO "====================="
+ECHO "Build with MSBuild..."
+ECHO "====================="
+
+msbuild /property:Configuration=Release
+
+@ECHO OFF
+ECHO "====================="
+ECHO "Release Binary..."
+ECHO "====================="
+
+SET OUTPUT_RKG_DIR=random-keygen-win32
+SET DEPLOY_FILE=%OUTPUT_RKG_DIR%.zip
+
+mkdir %OUTPUT_RKG_DIR%
+
+copy Release\RKG.exe %OUTPUT_RKG_DIR%
+copy Release\NF.dll %OUTPUT_RKG_DIR%
+copy MSVCX\msvcr100.dll %OUTPUT_RKG_DIR%
+copy MSVCX\msvcp100.dll %OUTPUT_RKG_DIR%
+copy readme.txt %OUTPUT_RKG_DIR%
+copy license.txt %OUTPUT_RKG_DIR%
+
+@ECHO OFF
+ECHO "====================="
+ECHO "Zip..."
+ECHO "====================="
+
+cd tools
+7za.exe a ../%DEPLOY_FILE% ../%OUTPUT_RKG_DIR%
+cd ../
+
