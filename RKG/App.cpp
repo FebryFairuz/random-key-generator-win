229db6c925fc5dbef99277a44f5557a5b5eec575
Equan Pr <eq@junwatu.com>
2013-04-24 15:26:27 +0700

    RKG main source files.

---
+++ RKG/App.cpp
@@ -0,0 +1,35 @@
+/**
+* NeverFear 1.0
+* Equan Pr.
+* (c) 2013
+*/
+#include <iostream>
+#include <string>
+
+#include "NFDLL.h"
+
+using namespace std;
+
+string pwd_str;
+
+int main(){	
+	NFDLL nfd;
+	nfd.info();
+	cout << "Generated Password :" << endl;
+	cout << endl;
+	cout << endl;
+
+	pwd_str = nfd.generatePassword();
+	cout << pwd_str;
+	
+	cout << endl;
+	cout << endl;
+
+	nfd.saveToClipboard(pwd_str);
+	nfd.saveToFile(pwd_str, "");
+	cout << endl;
+	cout << "Done" << endl;
+	system("PAUSE");
+	
+	return 0;
+}
